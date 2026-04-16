29a021dc0950152b2e69cc0b67021044cef66ff9
mystique-V <adrien.poikolo@gmail.com>
2026-04-16 10:32:49 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,15 @@
+const CACHE = 'dashboard-moto-v1';
+const FILES = ['/dashboard-moto/', '/dashboard-moto/index.html'];
+
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('boisdo-v1').then(c =>
-      c.addAll(['/', '/index.html'])
-    )
-  );
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
 });
 self.addEventListener('fetch', e => {
   e.respondWith(
