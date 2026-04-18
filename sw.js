3bbcba9845df9c4caeebf1cf986210fa6419b2cd
mystique-V <adrien.poikolo@gmail.com>
2026-04-18 23:02:28 +0400

    bump v12

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 // Version du cache : change ce numéro quand tu modifies ton app
 // pour forcer le rafraîchissement chez tes utilisateurs.
-const CACHE_VERSION = 'moto-ecole-v11';
+const CACHE_VERSION = 'moto-ecole-v12';
 
 // Liste des fichiers à mettre en cache dès l'installation de l'app.
 // Ce sont les fichiers essentiels pour que l'app fonctionne hors-ligne.
